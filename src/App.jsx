78513c624fa73b9564b0e9b9356334c1076b3bcf
disha510 <disha.raghugowda2007@gmail.com>
2025-12-22 22:04:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import Navbar from "./components/Navbar"
 import Home from "./sections/Home"
-import Career from "./sections/Career"
-import Stats from "./sections/Stats"
-import Media from "./sections/Media"
-import FanZone from "./sections/FanZone"
-import Timeline from "./sections/Timeline"
+
 function App() {
   return (
     <div>
       <Navbar/>
       <Home />
-      <Timeline/>
-      <Career />
-      <Stats />
-      <Media />
-      <FanZone />
+      
     </div>
   )
 }
