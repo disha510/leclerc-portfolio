27d24a506ed8df9ab8a8f5ba0ce10d79b7354cc5
Disha Gowda <dishagowda@Dishas-MacBook-Air-2.local>
2025-12-22 20:35:42 +0530

    Add timeline.js data file

---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export const timelineData = [
+  {
+    year: "2016",
+    title: "Karting Champion",
+    description: "Won his first international karting championship.",
+    image: "/images/2016.jpg"
+  },
+  {
+    year: "2018",
+    title: "Joined Sauber F1 Team",
+    description: "Made his Formula 1 debut with Sauber.",
+    image: "/images/2018.jpg"
+  },
+  {
+    year: "2019",
+    title: "First F1 Podium",
+    description: "Finished 3rd in Bahrain Grand Prix.",
+    image: "/images/2019.jpg"
+  },
+  {
+    year: "2022",
+    title: "Ferrari Contender",
+    description: "Multiple race wins and pole positions.",
+    image: "/images/2022.jpg"
+  }
+];
