3719265c5c83d9a5be7db364277b7e8ef213ee5a
disha510 <disha.raghugowda2007@gmail.com>
2025-12-23 19:46:58 +0530

    Update Home.jsx

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -13,14 +13,41 @@ export default function Home() {
         playsInline
         
       </div>
-      <div className="relative z-10 text-center text white px-4 max-w-3xl">
-      <h1 className="text-5xl md:text-6xl font-bold text-red-600 drop-shadow-xl fade-in"> </h1>
-      Charles Leclerc
-      <p className="text-xl md:text-2xl mt-4 italic text-white drop-shadow-md fade-in delay-300">
-      "Every race is a chance to push limits."
 
-      </div>  
+
+      {/*Overlay so that ppl can see*/}
+      <div className="absolute inset-0 bg-black/50"></div>
+      
+      {/*Text*/}
+      <div className="relative z-10 h-full flex flex-col items-center justify-center text-center px-4"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      >
+      <h1 
+      ref={titleRef}
+      className="text-6xl md:text-8xl uppercase font-title drop-shadow-xl fade-in
+                transition-transform duration-300 ease-in-out hover:scale-105 hover:drop-shadow-[0_0_25px_#FF2800]"
+      style={{color: "#FF2800" }}
+      > 
+      CHARLES LECLERC 
+      </h1>
+      
+      <p className="mt-4 text-2xl md:text-3xl italic font-subtitle text-white drop-shadow-md fade-in delay-300 hover:drop-shadow-[0_0_15px_#FF2800] transition-all duration-300 ease-in-out">
+      Every race is a chance to push limits
+    
+      </p>
+      
+
+      </div>
+      {/* Fadein animation */}
+      <style>{`
+      @keyframes fadeIn { 
+        from { opacity: 0; transform: translateY(20px);}
+        to { opacity: 1; transform: translateY(0);}
+      }
+      .fade-in { animation: fadeIn 1s ease forwards; }
+      .delay-300 { animation-delay: 0.3s;}
+      `}</style>
       </section>
-      )
+  )
 }
-
